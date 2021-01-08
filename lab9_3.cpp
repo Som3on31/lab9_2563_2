f13f63201ea4da263ff10adbce640f8ee317154d
Som3on31 <Som3on31_kek@users.noreply.github.com>
2021-01-08 16:34:18 +0700

    Completed the program.

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,3 +1,35 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    int count=0;
+    float sum,sd1;
+    string number;
+
+    ifstream source("score.txt");
+
+    while (getline(source,number)){
+        sum += atof(number.c_str());
+        sd1+=pow(atof(number.c_str()),2);
+        count++;
+    }
+    
+
+
+
+
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << sum/count << endl;
+    cout << "Standard deviation = "<< pow((sd1/count)-pow(sum/count,2),0.5) << endl;
+
+    source.close();
+
+    return 0;
+}
